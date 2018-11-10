f498115fa7a5b0d0f4f2b21e40493d63d108203e
hc <1412002323@qq.com>
2018-11-10 19:06:45 +0800

    some html template

---
+++ js/js.js
@@ -0,0 +1,16 @@
+    var oldcolor = "#EEAD0E";
+    var newcolor = "grey";
+    function bigger(id){
+        document.getElementById("butt"+id).style.backgroundColor = oldcolor;
+        }
+    function smaller(id){
+        document.getElementById("butt"+id).style.backgroundColor = newcolor;
+        }
+    var foldcolor = "#EEAD0E";
+    var fnewcolor = "white";
+    function fbigger(id){
+        document.getElementById("butt"+id).style.backgroundColor = foldcolor;
+        }
+    function fsmaller(id){
+        document.getElementById("butt"+id).style.backgroundColor = fnewcolor;
+        }
